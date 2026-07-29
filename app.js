0f6cf46153560e150a0391b01f251bf8e56fa93b
angelfarias867-wq <angelfarias867@gmail.com>
2026-07-29 19:51:02 -0400

    fuap

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const app = express();
+exports.app = app;
+const { MONGO_URI } = require("./config");
+
+(async() => {
+
+try {
+    //Se conecta a la base de datos
+await mongoose.connect(MONGO_URI)
+console.log("Conectado a Mongo DB");
+
+} catch (error) {
+console.log(error);
+}
+})()
+
+//Rutas Frontend
+app.use("/", express.static(path.resolve("views", "login")))
+
+
+
+module.exports = app;
